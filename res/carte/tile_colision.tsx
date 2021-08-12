de9c3528cdb06a9a77c809c5a1abe2a967ee535c
JCharlesLuans <luans.jeancharles@gmail.com>
2021-08-12 16:00:06 +0200

    Mise à jour Tiled

--- res/carte/tile_colision.tsx
+++ res/carte/tile_colision.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="collision_final" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+<tileset version="1.5" tiledversion="1.7.2" name="tile_collision" tilewidth="32" tileheight="32" tilecount="25" columns="5">
  <image source="texture/collision_final.png" width="160" height="160"/>
 </tileset>
