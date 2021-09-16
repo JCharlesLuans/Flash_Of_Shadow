08c9128d73789c205385627bc382b6f3a08a8168
JCharlesLuans <luans.jeancharles@gmail.com>
2021-09-16 09:10:13 +0200

    Positionnement du texte centré dans le bouton

--- src/org/thunderbot/FOS/client/statiqueState/layout/Bouton.java
+++ src/org/thunderbot/FOS/client/statiqueState/layout/Bouton.java
@@ -3,36 +3,27 @@
  */
 package org.thunderbot.FOS.client.statiqueState.layout;
 
+import org.newdawn.slick.Font;
 import org.newdawn.slick.Graphics;
 
-public class Bouton {
+public class Bouton extends Layout {
 
-    private int longueur;
-    private int hauteur;
-    private float x;
-    private float y;
-    private String texte;
-
-    public Bouton(int longeur, int hauteur, float x, float y, String texte) {
-        this.longueur = longeur;
-        this.hauteur = hauteur;
-        this.x = x;
-        this.y = y;
-        this.texte = texte;
+    public Bouton(float x, float y, int longeur, int hauteur, String texte) {
+        super(x, y, longeur, hauteur, texte);
     }
 
-    public void render(Graphics graphics) {
-        graphics.drawRect(x, y, longueur, hauteur);
-        graphics.drawString(texte, x+5, y+(hauteur/2-4));
-    }
-
-    /**
-     * @param x
-     * @param y
-     * @return true si lees coordonnée sont comprise dans le bouton, false sinon
-     */
     public boolean isInBouton(float x, float y) {
-        return this.x < x && x < this.x + this.longueur && this.y < y && y < this.y + longueur;
+        return isInLayout(x, y);
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+        Font font = graphics.getFont();
+
+        graphics.drawRect(x, y, longueur, hauteur);
+        graphics.drawString(texte,
+                         x + (float) (longueur / 2 - font.getWidth(texte) / 2),
+                         y + (float) (hauteur / 2 - font.getHeight(texte) / 2));
     }
 
 }
--- src/org/thunderbot/FOS/client/statiqueState/layout/Layout.java
+++ src/org/thunderbot/FOS/client/statiqueState/layout/Layout.java
@@ -1,2 +1,74 @@
-package org.thunderbot.FOS.client.statiqueState.layout;public class Layout {
+package org.thunderbot.FOS.client.statiqueState.layout;
+
+import org.newdawn.slick.Graphics;
+
+public abstract class Layout {
+    protected int longueur;
+    protected int hauteur;
+    protected float x;
+    protected float y;
+    protected String texte;
+
+    public Layout(float x, float y, int longeur, int hauteur,  String texte) {
+        this.longueur = longeur;
+        this.hauteur = hauteur;
+        this.x = x;
+        this.y = y;
+        this.texte = texte;
+    }
+
+    public void render(Graphics graphics) {
+        graphics.drawRect(x, y, longueur, hauteur);
+        graphics.drawString(texte, x + 5, y + (hauteur / 2 - 4));
+    }
+
+    /**
+     * @param x
+     * @param y
+     * @return true si lees coordonnée sont comprise dans le bouton, false sinon
+     */
+    public boolean isInLayout(float x, float y) {
+        return this.x < x && x < this.x + this.longueur
+                && this.y < y && y < this.y + hauteur;
+    }
+
+    public int getLongueur() {
+        return longueur;
+    }
+
+    public void setLongueur(int longueur) {
+        this.longueur = longueur;
+    }
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    public void setHauteur(int hauteur) {
+        this.hauteur = hauteur;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
+
+    public void setTexte(String texte) {
+        this.texte = texte;
+    }
 }
