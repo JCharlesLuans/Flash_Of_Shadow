1a3dd8868e9434514aca9542567d9bf218e8c017
JCharlesLuans <luans.jeancharles@gmail.com>
2021-09-22 21:17:40 +0200

    Mise en place de la classe representant les mobs

---
+++ src/org/thunderbot/FOS/client/gameState/entite/Mob.java
@@ -0,0 +1,14 @@
+/*
+ * Mob.java                 22/09/2021
+ * Copyright et copyleft TNLag Corp.
+ */
+
+package org.thunderbot.FOS.client.gameState.entite;
+
+/**
+ * TODO ecrire Java Doc
+ *
+ * @author Jean-Charles Luans
+ */
+public class Mob {
+}
