98b1a1374853b4676c92c4d0e1eeb803e897659f
JCharlesLuans <luans.jeancharles@gmail.com>
2021-09-23 10:18:31 +0200

    Ajout fonction pour get les personnage non joueur par leurs map

--- src/org/thunderbot/FOS/database/FosDAO.java
+++ src/org/thunderbot/FOS/database/FosDAO.java
@@ -408,4 +408,40 @@ public class FosDAO {
         Guilde aRetourner = new Guilde();
         return aRetourner;
     }
+
+
+
+    public ArrayList<PNJ> getPnjByIdmap(String id) {
+        ArrayList<PNJ> aRetourner = new ArrayList<>();
+        String requete =
+                "SELECT * FROM " + NOM_TABLE_PNJ + " WHERE " + PNJ_CLE_MAP + " = " + id + ";";
+        try {
+
+            ResultSet rs = gestionnaireBase.executeRequete(requete);
+            while (rs.next()) {
+                PNJ tmp = new PNJ();
+                tmp.setId(rs.getInt(PNJ_CLE));
+                tmp.setNom(rs.getString(PNJ_NOM));
+                tmp.setSprite(rs.getString(PNJ_SPRITE));
+                tmp.setAgressif(rs.getInt(PNJ_AGRESSIF));
+                tmp.setStatArmure(rs.getInt(PNJ_STAT_ARMURE));
+                tmp.setStatAgilite(rs.getInt(PNJ_STAT_AGILITE));
+                tmp.setStatDexterite(rs.getInt(PNJ_STAT_DEXTERITE));
+                tmp.setStatEndurance(rs.getInt(PNJ_STAT_ENDURANCE));
+                tmp.setStatForce(rs.getInt(PNJ_STAT_FORCE));
+                tmp.setStatInteligence(rs.getInt(PNJ_STAT_INTELLIGENCE));
+                tmp.setStatSagesse(rs.getInt(PNJ_STAT_SAGESSE));
+                tmp.setIdMap(rs.getInt(PNJ_CLE_MAP));
+                tmp.setIdFaction(rs.getInt(PNJ_CLE_FACTION));
+                tmp.setIdTitre(rs.getInt(PNJ_CLE_TITRE));
+                aRetourner.add(tmp);
+            }
+            rs.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return aRetourner;
+    }
 }
